073cbe2c0754a34c8ea76a056229d3ced5df49f1
Joshua Taillon <jat255@gmail.com>
2018-05-31 12:58:20 -0400

    add docstrings to code

--- pythonfinancier/easycouchdb.py
+++ pythonfinancier/easycouchdb.py
@@ -1,10 +1,19 @@
+"""
+A module for more easily interacting with a CouchDB instance. Handles
+login, querying, listing all documents, and inserting/saving values
+"""
+
 import requests
-from urllib.parse import urljoin, urlunsplit
+from urllib.parse import urljoin
 from time import sleep
 from requests.adapters import HTTPAdapter
 
 
 class EasyCouchdb:
+    """
+    Easily get, insert, and update documents within a CouchDB database.
+    """
+
     SESSION = '_session'
     ALL_DBS = '_all_dbs'
     ALL_DOCS = '_all_docs'
@@ -14,12 +23,31 @@ class EasyCouchdb:
     req_session = None
 
     def __init__(self, url):
+        """
+        Create an EasyCouchdb instance
+
+        Parameters
+        ----------
+        url : str
+        """
         self.url = url
         self.SESSION_URL = urljoin(self.url, self.SESSION)
         self.ALL_DBS_URL = urljoin(self.url, self.ALL_DBS)
         print(self.SESSION_URL)
 
     def login(self, username, password):
+        """
+        Login to the database
+
+        Parameters
+        ----------
+        username : str
+        password : str
+
+        Returns
+        -------
+            Response to the login POST request
+        """
         self.req_session = requests.session()
         self.req_session.mount(self.url, HTTPAdapter(max_retries=5))
         return self.req_session.post(self.SESSION_URL,
@@ -28,11 +56,38 @@ class EasyCouchdb:
                                      timeout=self.TIMEOUT)
 
     def all_docs(self, db_name):
+        """
+        Get all documents from the CouchDB instance
+
+        Parameters
+        ----------
+        db_name : str
+            Name of the database to query
+
+        Returns
+        -------
+            Response to the all_docs GET request
+        """
         return self.req_session.get(
             urljoin(self.url, '/'.join([db_name, self.ALL_DOCS])),
             timeout=self.TIMEOUT)
 
     def query(self, db_name, selector):
+        """
+        Query the database with a given selector
+
+        Parameters
+        ----------
+        db_name : str
+            Name of the database to query
+        selector : dict
+            Selector to use for the query
+
+        Returns
+        -------
+        ret : list
+            List of the documents that match the given query
+        """
         sleep(0.5)
         print('executing query: {0}'.format(selector))
         ret = self.req_session.post(
@@ -42,16 +97,58 @@ class EasyCouchdb:
         return ret
 
     def insert(self, db_name, doc):
+        """
+        Insert a document into the database
+
+        Parameters
+        ----------
+        db_name : str
+            Name of the database into which to insert
+        doc : dict
+            JSON-formatted dictionary of the document to add
+
+        Returns
+        -------
+            Response of the POST request for the insertion
+        """
         sleep(0.5)
         return self.req_session.post(urljoin(self.url, db_name), json=doc,
                                      timeout=self.TIMEOUT)
 
     def save(self, db_name, doc):
+        """
+        Update a document with a given id in the database
+
+        Parameters
+        ----------
+        db_name : str
+            Name of the database into which to insert
+        doc : dict
+            JSON-formatted dictionary of the document to add
+
+        Returns
+        -------
+            Response of the PUT request for the document update
+        """
         sleep(0.5)
         return self.req_session.put(
             urljoin(self.url, '/'.join([db_name, doc['_id']])), json=doc)
 
     def get_doc(self, db_name, _id):
+        """
+        Get a document from the database with a given id value
+
+        Parameters
+        ----------
+        db_name : str
+            Name of the database from which to get the document
+        _id : str
+            ID value of the document
+
+        Returns
+        -------
+            Response of the GET request for the document retrieval
+        """
         sleep(0.5)
         return self.req_session.get(
             urljoin(self.url, '/'.join([db_name, _id])),
--- pythonfinancier/financier.py
+++ pythonfinancier/financier.py
@@ -1,16 +1,52 @@
+"""
+This module allows you insert transactions and payees on Financier
+"""
+
 from pythonfinancier.easycouchdb import EasyCouchdb
 import uuid
 
 
 def split_id(full_id):
+    """
+    Split an id value to get the last part of the string
+
+    Parameters
+    ----------
+    full_id : str
+        The full id value containing the budget, account, and transaction (
+        etc.) information
+    Returns
+    -------
+        Just the last part of the id value (which is usually the part of
+        interest)
+    """
     return full_id.split('_')[-1]
 
 
 class Financier:
+    """
+    A class to more easily interact with a Financier user's database
+    """
+
     selector = {}
 
-    def __init__(self, urlcouchdb, username, password):
-        self.cdb = EasyCouchdb(urlcouchdb)
+    def __init__(self, url_couch_db, username, password):
+        """
+        Create a new instance of the Financier class
+
+        Parameters
+        ----------
+        url_couch_db : None or str
+            If None, value is read from config file 'python-financier.ini'
+            If string, Web url of the Financier database to which to connect
+        username : None or str
+            If None, value is read from config file 'python-financier.ini'
+            If string, username to login with
+        password : None or str
+            If None, value is read from config file 'python-financier.ini'
+            If string, password to login with
+        """
+        self.cdb = EasyCouchdb(url_couch_db)
         # print(self.cdb.login(username, password).json())
         login_json = self.cdb.login(username, password).json()
         roles = login_json['roles']
@@ -22,11 +58,27 @@ class Financier:
         print('Connecting on db {0}'.format(self.user_db))
 
     def get_all_budgets(self):
+        """
+        Get all the budgets that exist in this Financier database
+
+        Returns
+        -------
+            List of the budgets as json objects, each containing the ``name``
+            and ``_id`` of the budget
+        """
         return self.cdb.query(self.user_db,
                               {'selector': {'_id': {'$regex': '^budget_'}},
                                'fields': ['_id', 'name']}).json()['docs']
 
     def connect_budget(self, name):
+        """
+        Set a particular budget as "active" within the Financier class
+
+        Parameters
+        ----------
+        name : str
+            Name of the budget to use (must be an exact match)
+        """
         budget = self.find_budget(name)
         if budget:
             self.budget_selector = budget[0]['_id'].replace('budget', 'b')
@@ -35,14 +87,47 @@ class Financier:
             raise Exception('Budget not found')
 
     def get_all_accounts(self):
+        """
+        Get a list of all accounts present within the active budget
+
+        Returns
+        -------
+            List of accounts contained in the active budget
+        """
         selector = {
             '_id': {'$regex': '^{0}_account_'.format(self.budget_selector)}}
         return self.cdb.query(self.user_db,
                               {'selector': selector,
                                'fields': ['_id', 'name']}).json()['docs']
 
-    def save_transaction(self, account_name, this_id, value, date, payee_name,
-                         memo):
+    def save_transaction(self, account_name, this_id,
+                         value, date, payee_name, memo):
+        """
+        Add a transaction to the database of the active budget. Category for
+        transaction will be automatically determined from the autosuggest value
+        for the particular payee.
+
+        Parameters
+        ----------
+        account_name : str
+            Name of the account to use
+        this_id : str
+            A UUID4 formatted id to use for the transaction
+        value : float or int
+            The value of the transaction (positive for inflow, negative for
+            outflow)
+        date : str
+            The date of the transaction (formatted YYYY-MM-DD)
+        payee_name : str
+            Name of the payee to use (will be created if it does not
+            already exist)
+        memo : str
+            Memo to save in the transaction
+
+        Returns
+        -------
+            JSON response of the database upon inserting the transaction
+        """
         # getting account
         # first check if already in cache map
         if account_name not in self.account_map:
@@ -81,17 +166,66 @@ class Financier:
                 'transaction {0} has already been imported '.format(tr['_id']))
 
     def get_transaction(self, id_transaction):
+        """
+        Get the JSON object representing a transaction
+
+        Parameters
+        ----------
+        id_transaction : str
+            transaction id of the form
+            "<BUDGET UUID4>_transaction_<TRANSACTION UUID4>"
+
+        Returns
+        -------
+            JSON representation of the transaction object in the database
+        """
         return self.cdb.get_doc(self.user_db, id_transaction).json()
 
     def get_id_transaction(self, this_id):
+        """
+        Helper function to build a valid transaction id from the currently
+        selected budget and a custom UUID4 value
+
+        Parameters
+        ----------
+        this_id : str
+            UUID4-formatted id for a particular transaction
+
+        Returns
+        -------
+            String of the form "<BUDGET UUID4>_transaction_<TRANSACTION UUID4>"
+            that can be used directly in the database
+        """
         return '{0}_transaction_{1}'.format(self.budget_selector, this_id)
 
     def find_budget(self, name):
+        """
+        Find budget(s) by name within the database
+
+        Parameters
+        ----------
+        name : str
+            Name for which to search (must be an exact match)
+        Returns
+        -------
+            List of account json objects that match the given name
+        """
         return self.cdb.query(self.user_db, {
             'selector': {'_id': {'$regex': '^budget_'}, 'name': name},
             'fields': ['_id', 'name']}).json()['docs']
 
     def find_account(self, name):
+        """
+        Find account(s) by name within the database
+
+        Parameters
+        ----------
+        name : str
+            Name for which to search (must be an exact match)
+        Returns
+        -------
+            List of account json objects that match the given name
+        """
         selector = {
             '_id': {'$regex': '^{0}_account_'.format(self.budget_selector)},
             'name': name}
@@ -100,6 +234,17 @@ class Financier:
                                'fields': ['_id', 'name']}).json()['docs']
 
     def find_payee(self, name):
+        """
+        Find payee(s) by name within the database
+
+        Parameters
+        ----------
+        name : str
+            Name for which to search (must be an exact match)
+        Returns
+        -------
+            List of payee json objects that match the given name
+        """
         selector = {
             '_id': {'$regex': '^{0}_payee_'.format(self.budget_selector)},
             'name': name}
@@ -109,13 +254,36 @@ class Financier:
                                           'categorySuggest']}).json()['docs']
 
     def insert_payee(self, name):
+        """
+        Insert a payee into the database with the given name
+
+        Parameters
+        ----------
+        name : str
+
+        Returns
+        -------
+            Response from the insert request
+        """
         doc = {
             '_id': '{0}_payee_{1}'.format(self.budget_selector, uuid.uuid4()),
             'name': name, 'internal': False, 'autosuggest': True}
         return self.cdb.insert(self.user_db, doc).json()
 
-    # return a id of payee, if not exists create a new one
     def get_or_create_payee(self, name):
+        """
+        Return id of a payee, and create one if it does not exist
+
+        Parameters
+        ----------
+        name : str
+            Name of the payee to use or get
+
+        Returns
+        -------
+        ret : str
+            The ``id`` value of the existing (or created) payee
+        """
         payee = self.find_payee(name)
         if payee:
             return payee[0]
