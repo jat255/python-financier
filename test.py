25f01a1ba2152dfed2f1de3c8b08ea5e41ceecdd
Joshua Taillon <jat255@gmail.com>
2018-06-01 14:49:29 -0400

    add test file

---
+++ test.py
@@ -0,0 +1,82 @@
+import logging
+
+from pythonfinancier import Financier
+from datetime import datetime
+from pprint import pprint
+
+# logging.getLogger().setLevel(logging.INFO)
+logger1 = logging.getLogger('pythonfinancier.financier')
+logger2 = logging.getLogger('pythonfinancier.easycouchdb')
+logger1.setLevel(logging.INFO)
+logger2.setLevel(logging.INFO)
+logging.info('Get logging setup')
+
+f = Financier(conf_file='my_financier.ini')
+f.connect_budget('Our Finances')
+pprint(f.find_transaction(value=200000, date='2018-05-26'))
+pprint(f.find_transaction(value=-200000, date='2018-05-26'))
+today = datetime.now().strftime('%Y-%m-%d')
+
+
+def test_saving():
+    # shouldn't do any query for income category:
+    f.save_transaction(account_name='Test from python',
+                       category_name='income',
+                       value=20000,
+                       date=today,
+                       payee_name='Payee1',
+                       memo='Christmas memo')
+
+    # should query remote db for category id:
+    f.save_transaction(account_name='Test from python',
+                       category_name='Christmas',
+                       value=-5000,
+                       date=today,
+                       payee_name='Payee2',
+                       memo='Christmas memo')
+
+    # should get category from map:
+    f.save_transaction(account_name='Test from python',
+                       category_name='Christmas',
+                       value=-5000,
+                       date=today,
+                       payee_name='Payee3',
+                       memo='Christmas memo')
+
+    # should get category and payee from map:
+    f.save_transaction(account_name='Test from python',
+                       category_name='Rent/Mortgage',
+                       value=-5000,
+                       date=today,
+                       payee_name='Payee1',
+                       memo='Christmas memo')
+
+
+def test_splits():
+    # each dict in the list must have keys [value, category_name, memo,
+    # payee_name]
+    split_transactions = [{'value':10000,
+                           'payee_name': 'split_payee1',
+                           'category_name': 'Rent/Mortgage',
+                           'memo': 'split 1/3'},
+                          {'value': 5000,
+                           'payee_name': 'split_payee2',
+                           'category_name': 'Clothing',
+                           'memo': 'split 2/3'},
+                          {'value': 3000,
+                           'payee_name': 'split_payee1',
+                           'category_name': 'incomeNextMonth',
+                           'memo': 'split 3/3'}]
+
+    f.save_split(account_name='Test from python',
+                 payee_name='Payee1',
+                 value=20000,
+                 date=today,
+                 memo='testing splits',
+                 transactions=split_transactions)
+
+
+def test_transfer():
+    pass
+
+# test_splits()
